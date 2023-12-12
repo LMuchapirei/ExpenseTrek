d1bd9c1e4abc4fef64c8ef588810dfc2b0295c5e
LMuchapirei <h170407h@hit.ac.zw>
2023-12-12 10:31:30 +0200

    Initial Commit

---
+++ ExpenseTrek/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseTrek
+//
+//  Created by Linval Muchapirei on 12/12/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExpenseTrek/ExpenseTrekApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrekApp.swift
+//  ExpenseTrek
+//
+//  Created by Linval Muchapirei on 12/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
