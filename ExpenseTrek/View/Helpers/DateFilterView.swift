55c93cb69dd3c435bb9a725cd56491cdd76e0e28
LMuchapirei <h170407h@hit.ac.zw>
2023-12-15 13:33:34 +0200

    Add the filtering logic

--- ExpenseTrek/View/Helpers/DateFilterView.swift
+++ ExpenseTrek/View/Helpers/DateFilterView.swift
@@ -8,12 +8,36 @@
 import SwiftUI
 
 struct DateFilterView: View {
-    @State private var start: Date
-    @State private var end: Date
+    @State var start: Date
+    @State var end: Date
     var onSubmit:(Date,Date)->()
-    var onClose: ()-> ()
+    var onClose: () -> ()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing:15){
+            DatePicker("Start Date",selection: $start,displayedComponents: [.date])
+            DatePicker("End Date",selection: $end,displayedComponents: [.date])
+            
+            HStack (spacing:15){
+                Button("Cancel"){
+                    onClose()
+                }
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.roundedRectangle(radius: 5))
+                .tint(.red)
+                
+                Button("Filter"){
+                    onSubmit(start,end)
+                }
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.roundedRectangle(radius: 5))
+                .tint(appTint)
+            }
+            .padding(.top,10)
+        }
+        .padding(15)
+        .background(.bar,in: .rect(cornerRadius: 10))
+        .padding(.horizontal,30)
+        
     }
 }
 
---
+++ ExpenseTrek/View/Helpers/LockView.swift
@@ -0,0 +1,18 @@
+//
+//  LockView.swift
+//  ExpenseTrek
+//
+//  Created by Linval Muchapirei on 15/12/2023.
+//
+
+import SwiftUI
+
+struct LockView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LockView()
+}
