f706fd341baff41cd4bf719330a6d4db6d7987b0
LMuchapirei <h170407h@hit.ac.zw>
2023-12-18 12:51:26 +0200

    Add edit option for save,wire the save function to model container

--- ExpenseTrek/View/NewExpenseView.swift
+++ ExpenseTrek/View/NewExpenseView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct NewExpenseView: View {
+    ///Env Properties
+    @Environment(\.modelContext) private var context
+    @Environment(\.dismiss) private var dismiss
     /// View Properties
     @State private var title: String = ""
     @State private var remarks: String = ""
     @State private var amount: Double = .zero
     @State private var dateAdded: Date = .now
     @State private var category: Category = .expense
+    var editTransaction: Transaction?
     /// Random Tint
-    var tint: TintColor = tints.randomElement()!
+    @State var tint: TintColor = tints.randomElement()!
     
     var body: some View {
         ScrollView{
@@ -40,13 +44,15 @@ struct NewExpenseView: View {
                         .hSpacing(.leading)
                     
                     HStack(spacing:15){
-                        TextField("0.0",value:$amount,formatter: numberFormatter)
-                            .padding(.horizontal,15)
+                        HStack(spacing:4){
+                            Text(currencySymbol)
+                                .font(.callout.bold())
+                            TextField("0.0",value:$amount,formatter: numberFormatter)
+                                .keyboardType(.decimalPad)
+                           }.padding(.horizontal,15)
                             .padding(.vertical,12)
                             .background(.background,in:.rect(cornerRadius: 10))
                             .frame(maxWidth:130)
-                            .keyboardType(.decimalPad)
-                        
                         /// Custom Check Box
                         CategoryCheckBox()
                     }
@@ -67,19 +73,49 @@ struct NewExpenseView: View {
             }
             .padding(15)
         }
-            .navigationTitle("Add Transaction")
+        .navigationTitle("\(editTransaction == nil ? "Add":"Edit") Transaction")
             .background(.gray.opacity(0.15))
             .toolbar(content: {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Save",action:{
-                        
+                        save()
                     })
+                }
+            })
+            .onAppear(perform: {
+                if let editTransaction {
+                    /// Load All Existing Data from the Transaction
+                    title = editTransaction.title
+                    remarks = editTransaction.remarks
+                    dateAdded = editTransaction.dateAdded
+                    if let category = editTransaction.rawCategory {
+                        self.category = category
+                    }
+                    if let tint = editTransaction.tint {
+                        self.tint = tint
+                    }
+                    amount = editTransaction.amount
+
                 }
             })
     }
     
     /// Saving Data
     func save(){
+        /// Saving Item to SwiftData
+        if editTransaction != nil {
+            editTransaction?.title = title
+            editTransaction?.remarks = remarks
+            editTransaction?.amount = amount
+            editTransaction?.category = category.rawValue
+            editTransaction?.dateAdded = dateAdded
+            
+        } else {
+            let transaction = Transaction(title: title, remarks: remarks, amount: amount, dateAdded: dateAdded, category: category, tintColor: tint)
+            
+            context.insert(transaction)
+        }
+        dismiss()
         
     }
     
